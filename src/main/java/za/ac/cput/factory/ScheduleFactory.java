2c3060577642893d32b714991d22caf266aeb8e7
OkuhleV <102515607+OkuhleV@users.noreply.github.com>
2024-05-18 18:29:26 +0200

    Update ScheduleFactory.java

--- src/main/java/za/ac/cput/factory/ScheduleFactory.java
+++ src/main/java/za/ac/cput/factory/ScheduleFactory.java
@@ -4,5 +4,25 @@ package za.ac.cput.factory;
 
  */
 
+import za.ac.cput.domain.Schedule;
+
+import java.sql.Time;
+import java.util.Date;
+
 public class ScheduleFactory {
+
+
+    public static Schedule createSchedule(String employeeId, String scheduleId,
+                                          Date scheduleDate, Time startTime, Time endtime){
+        return new Schedule.Builder()
+                .setEmployeeId(employeeId)
+                .setScheduleId(scheduleId)
+                .setScheduleDate(scheduleDate)
+                .setStartTime(startTime)
+                .setEndTime(endtime)
+                .build();
+    }
+
+
+
 }
