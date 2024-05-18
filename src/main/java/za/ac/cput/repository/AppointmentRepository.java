a11c2d73988d20efd47747d62e6c70f2f52c75d8
Sinentlahla219081328 <sinentlahlapindani@gmail.com>
2024-05-18 15:59:29 +0200

    Sales and Appointment Repository added

---
+++ src/main/java/za/ac/cput/repository/AppointmentRepository.java
@@ -0,0 +1,11 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Appointment;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, String> {
+    Appointment findAppointmentsByClientID(String clientID);
+}
+
