fbcd8cee1ad77291cda884fcff3fed33eaffeabb
Sinentlahla219081328 <sinentlahlapindani@gmail.com>
2024-05-18 14:20:58 +0200

    AppointmentFactory class and its factoryTest added

--- src/main/java/za/ac/cput/factory/AppointmentFactory.java
+++ src/main/java/za/ac/cput/factory/AppointmentFactory.java
@@ -4,5 +4,27 @@ package za.ac.cput.factory;
 
  */
 
+import za.ac.cput.domain.Appointment;
+
+import za.ac.cput.util.Helper;
+
+import java.sql.Time;
+import java.util.Date;
+
 public class AppointmentFactory {
+    public static Appointment createAppointment(String appointmentID, String clientID, String employeeID, Date date, Time time ){
+        if (Helper.isNullOrEmpty(appointmentID)
+                || Helper.isNullOrEmpty(clientID)
+                ||Helper.isNullOrEmpty(employeeID))
+
+            return null;
+
+        return new Appointment.Builder().setAppointmentID(appointmentID)
+                .setClientID(clientID)
+                .setEmployeeID(employeeID)
+                .setDate(date)
+                .setTime(time)
+                .build();
+    }
+
 }
---
+++ src/test/java/za/ac/cput/factory/AppointmentFactoryTest.java
@@ -0,0 +1,22 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Appointment;
+
+import java.sql.Time;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AppointmentFactoryTest {
+@Test
+    public void testCreateAppointment(){
+
+    Date date = new Date();
+    Time time = new Time(10,30,0);
+
+    Appointment appointment = AppointmentFactory.createAppointment("A123","C123","E123",date,time);
+    assertNotNull(appointment);
+    System.out.println(appointment.toString());
+}
+
+}
