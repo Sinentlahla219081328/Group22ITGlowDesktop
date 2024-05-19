3d7dcf7c5e3ef8cd8354f52641e768a9967faa33
Aphelele218130260 <218130260@mycput.ac.za>
2024-05-19 23:24:07 +0200

    employeef

--- src/main/java/za/ac/cput/factory/EmployeeFactory.java
+++ src/main/java/za/ac/cput/factory/EmployeeFactory.java
@@ -8,19 +8,26 @@ import za.ac.cput.domain.Employee;
 import za.ac.cput.util.Helper;
 
 public class EmployeeFactory {
-    public static Employee buildEmployee(String employeeID, String firstName, String lastName, String userName,Contact contact){
+    public static Employee buildEmployee(String employeeID, String firstName, String lastName, String userName,
+                                         String email,String mobileNumber,String workTelephone){
         if (Helper.isNullOrEmpty(employeeID) || Helper.isNullOrEmpty(firstName)
                 || Helper.isNullOrEmpty(lastName)|| Helper.isNullOrEmpty(userName) ||
-                Helper.isNullOrEmpty(String.valueOf(contact)))
+                Helper.isNullOrEmpty(mobileNumber) ||Helper.isNullOrEmpty(workTelephone) ||Helper.isNullOrEmpty(email))
+
+                //Helper.isNullOrEmpty(String.valueOf(contact)))
             return null;
         return new Employee.Builder().setEmployeeID(employeeID)
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .setUserName(userName)
-                .setContact(contact)
+                .setEmail(email)
+                .setMobileNumber(mobileNumber)
+                .setWorkTelephone(workTelephone)
+              //  .setContact(contact)
                 .build();
     }
-    public static Employee buildEmployee(String firstName, String lastName, String userName,Contact contact){
+    public static Employee buildEmployee(String firstName, String lastName, String userName,
+                                         String email,String mobileNumber,String workTelephone){
         if (Helper.isNullOrEmpty(firstName)
                 || Helper.isNullOrEmpty(userName))
             return null;
@@ -29,7 +36,10 @@ public class EmployeeFactory {
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .setUserName(userName)
-                .setContact(contact)
+                .setEmail(email)
+                .setMobileNumber(mobileNumber)
+                .setWorkTelephone(workTelephone)
+
                 .build();
     }
 }
