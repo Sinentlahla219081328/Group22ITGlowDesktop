6f54e109562147e8ceffc5c3c4b87723ead90937
Administrator <rolomanasiyamthanda@gmail.com>
2024-05-19 22:02:11 -0700

    i am done with the work you can merge i have tested them

--- src/main/java/za/ac/cput/repository/PaymentRepository.java
+++ src/main/java/za/ac/cput/repository/PaymentRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Payment;
 
+import java.util.List;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, String> {
+    List<Payment> findByClientClientId(String clientId);
+
 }
