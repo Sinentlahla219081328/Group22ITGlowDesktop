2df0129d5b4291b84b4d67b46afc5ad8d5da8b99
Najeeb Abdi <najeebwarsame@gmail.com>
2024-05-21 14:07:50 -0700

    Helper class update

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,27 +1,31 @@
 package za.ac.cput.util;
 
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
+import org.apache.commons.validator.routines.EmailValidator;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class Helper {
+
     public static boolean isNullOrEmpty(String s) {
         if (s == null|| s.isEmpty())
             return true;
         return false;
     }
+
     public static String generateId(){
+
         return UUID.randomUUID().toString();
     }
 
     public static boolean isValidEmail(String email) {
-        boolean result = true;
-        try {
-            InternetAddress emailAddr = new InternetAddress(email);
-            emailAddr.validate();
-        } catch (AddressException ex) {
-            result = false;
-        }
-        return result;
+        EmailValidator validator = EmailValidator.getInstance();
+        return validator.isValid(email);
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        String regex = "\\d{10}";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(phoneNumber).matches();
+
     }
 }
